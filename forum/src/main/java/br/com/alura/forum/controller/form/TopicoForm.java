ff937159a1f1979e1d7e2b54a8e548d74fba1603
Gabriel Soares <gsoaresdesouza@live.com>
2022-08-02 19:39:52 -0300

    Bean Validation

--- forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
+++ forum/src/main/java/br/com/alura/forum/controller/form/TopicoForm.java
@@ -1,14 +1,26 @@
 package br.com.alura.forum.controller.form;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.modelo.Topico;
 import br.com.alura.forum.repository.CursoRepository;
-import br.com.alura.forum.repository.TopicoRepository;
 
 public class TopicoForm {
 
+	@NotNull
+	@NotEmpty
+	@Length(min = 5)
 	private String titulo;
+	@NotNull
+	@NotEmpty
+	@Length(min = 10)
 	private String mensagem;
+	@NotNull
+	@NotEmpty	
 	private String nomeCurso;
 
 	public String getTitulo() {
@@ -36,7 +48,7 @@ public class TopicoForm {
 	}
 
 	public Topico converter(CursoRepository cursoRepository) {
-		
+
 		Curso curso = cursoRepository.findByNome(nomeCurso);
 
 		return new Topico(titulo, mensagem, curso);
